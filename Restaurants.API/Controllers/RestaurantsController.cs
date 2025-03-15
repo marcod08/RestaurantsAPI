e344f8342330de043ceebaaf93a94e908954f986
marcod08 <marcodimprima91@gmail.com>
2025-03-15 17:49:30 +0100

    Creating a restaurant

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Restaurants;
+using Restaurants.Application.Restaurants.Dtos;
 
 namespace Restaurants.API.Controllers;
 
@@ -25,5 +26,13 @@ public class RestaurantsController(IRestaurantsService restaurantsService) : Con
         }
 
         return Ok(restaurant);
+    }
+
+    [HttpPost]
+
+    public async Task<IActionResult> CreateRestaurant([FromBody] CreateRestaurantDto createRestaurantDto)
+    {
+        int id = await restaurantsService.Create(createRestaurantDto);
+        return CreatedAtAction(nameof(GetById), new { id }, null);
     }
 }
--- Restaurants.Application/Restaurants/Dtos/RestaurantsProfile.cs
+++ Restaurants.Application/Restaurants/Dtos/RestaurantsProfile.cs
@@ -12,5 +12,13 @@ public class RestaurantsProfile : Profile
             .ForMember(d => d.Street, opt => opt.MapFrom(src => src.Address == null ? null : src.Address.Street))
             .ForMember(d => d.PostalCode, opt => opt.MapFrom(src => src.Address == null ? null : src.Address.PostalCode))
             .ForMember(d => d.Dishes, opt => opt.MapFrom(src => src.Dishes));
+
+        CreateMap<CreateRestaurantDto, Restaurant>()
+            .ForMember(d => d.Address, opt => opt.MapFrom(src => new Address
+            {
+                City = src.City,
+                Street = src.Street,
+                PostalCode = src.PostalCode,
+            }));
     }
 }

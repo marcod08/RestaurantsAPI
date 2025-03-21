b8f71b907686f9441d7c6257d5dc0d894e28dc3e
marcod08 <marcodimprima91@gmail.com>
2025-03-21 17:36:30 +0100

    sorting result

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurant/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurant;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+    private int[] allowPageSizes = [2, 3, 4];
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(r => r.PageSize)
+            .Must(value => allowPageSizes.Contains(value))
+            .WithMessage($"Page size must be in [{string.Join(",", allowPageSizes)}]");
+    }
+}

3aa9126ce5970a1dd3ea7594343ce58c3be420f1
marcod08 <marcodimprima91@gmail.com>
2025-03-22 12:46:28 +0100

    current user test using xUnit and FluentAssertion

---
+++ Restaurants.ApplicationTests/Users/CurrentUserTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Restaurants.Domain.Constants;
+using Xunit;
+
+namespace Restaurants.Application.Users.Tests;
+
+public class CurrentUserTests
+{
+    // TestMethod_Scenario_ExpectedResult
+    [Theory()]
+    [InlineData(UserRoles.Admin)]
+    [InlineData(UserRoles.User)]
+    public void isInRole_WithMatchingRole_ShouldReturnTrue(string roleName)
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // act
+        var isInRole = currentUser.isInRole(roleName);
+
+        // assert
+        isInRole.Should().BeTrue();
+    }
+
+    [Fact()]
+    public void isInRole_WithNoMatchingRole_ShouldReturnFalse()
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // act
+        var isInRole = currentUser.isInRole(UserRoles.Owner);
+
+        // assert
+        isInRole.Should().BeFalse();
+    }
+
+    [Fact()]
+    public void isInRole_WithNoMatchingRoleCase_ShouldReturnFalse()
+    {
+        // arrange
+        var currentUser = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User], null, null);
+
+        // act
+        var isInRole = currentUser.isInRole(UserRoles.Admin.ToLower());
+
+        // assert
+        isInRole.Should().BeFalse();
+    }
+}

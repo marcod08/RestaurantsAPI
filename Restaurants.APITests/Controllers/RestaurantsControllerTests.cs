19d9021ba16dbc595df9bca0f2d2ba7913c6df4d
marcod08 <marcodimprima91@gmail.com>
2025-03-23 11:26:29 +0100

    testing endpoints with authorization

--- Restaurants.APITests/Controllers/RestaurantsControllerTests.cs
+++ Restaurants.APITests/Controllers/RestaurantsControllerTests.cs
@@ -1,6 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
 using Xunit;
 
 namespace Restaurants.APITests.Controllers;
@@ -8,10 +18,63 @@ namespace Restaurants.APITests.Controllers;
 public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Program>>
 {
     private readonly WebApplicationFactory<Program> _factory;
+    private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
 
     public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
     {
-        _factory = factory;
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository), _ => _restaurantsRepositoryMock.Object));
+            });
+        });
+    }
+
+    [Fact()]
+    public async Task GetById_ForNonExistingId_ShouldReturn404NotFound()
+    {
+        //arrange
+        var id = 9999;
+
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync((Restaurant?)null);
+
+        var client = _factory.CreateClient();
+
+        //act
+        var response = await client.GetAsync($"/api/restaurants/{id}");
+
+        //assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
+    [Fact()]
+    public async Task GetById_ForExistingId_ShouldReturn200Ok()
+    {
+        //arrange
+        var id = 99;
+
+        var restaurant = new Restaurant()
+        {
+            Id = id,
+            Name = "Test",
+            Description = "Test Description",
+        };
+
+        _restaurantsRepositoryMock.Setup(m => m.GetByIdAsync(id)).ReturnsAsync(restaurant);
+
+        var client = _factory.CreateClient();
+
+        //act
+        var response = await client.GetAsync($"/api/restaurants/{id}");
+        var restaurantDto = await response.Content.ReadFromJsonAsync<RestaurantDto>();
+
+        //assert
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        restaurantDto.Should().NotBeNull();
+        restaurantDto.Name.Should().Be("Test");
+        restaurantDto.Description.Should().Be("Test Description");
     }
 
     [Fact()]
